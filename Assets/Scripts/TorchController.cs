8132f655b2cea43f1689072b096b9878299f4122
scatter-dev <christianlegge888@gmail.com>
2020-08-19 15:01:39 -0400

    flickering torches, walls

---
+++ Assets/Scripts/TorchController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchController : MonoBehaviour
+{
+	Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		light = GetComponent<Light>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		light.intensity = Mathf.Sin(Time.time * 5) * 3 + 12 ;
+    }
+}
